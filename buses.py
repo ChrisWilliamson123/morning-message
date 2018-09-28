cd60a1ab746bb3940548d888ab5c656a3e2df704
Chris Williamson <chris.williamson@bbc.co.uk>
2018-09-28 12:47:40 +0100

    Added args to bus script

--- buses.py
+++ buses.py
@@ -32,23 +32,36 @@ data = lambda bus_stop: {
    }
 }
 
+stops = {
+    'work': {
+        'id': '1800NF09811',
+        'destination_name': 'Salford Central'
+    },
+    'home': {
+        'id': '1800NF03991',
+        'destination_name': 'Media City UK'
+    }
+}
+
 british_time = pytz.timezone('Europe/London')
 
-get_live_buses = lambda all_buses: [b for b in all_buses['stopMonitors']['stopMonitor'][0]['monitoredCalls']['monitoredCall'] if 'expectedArrivalTime' in b]
+get_live_buses = lambda all_buses: [b for b in all_buses['stopMonitors']['stopMonitor'][0]['monitoredCalls']['monitoredCall'] if ('expectedArrivalTime' in b or 'expectedDepartureTime' in b)]
 
 def json_print(to_print):
     print(json.dumps(to_print, indent=4))
 
 
 def get_arrival_time(bus):
-    live_arrival_time_utc = datetime.datetime.strptime(bus['expectedArrivalTime'], '%Y-%m-%dT%H:%M:%SZ')
+    if 'expectedArrivalTime' in bus:
+        live_arrival_time_utc = datetime.datetime.strptime(bus['expectedArrivalTime'], '%Y-%m-%dT%H:%M:%SZ')
+    else:
+        live_arrival_time_utc = datetime.datetime.strptime(bus['expectedDepartureTime'], '%Y-%m-%dT%H:%M:%SZ')
     arrival_time_british = live_arrival_time_utc.replace(tzinfo=pytz.utc).astimezone(british_time)
     return arrival_time_british
 
 def get_difference_in_minutes_between(time_a, time_b):
     difference = time_a - time_b
     in_minutes_and_seconds = divmod(difference.days * 86400 + difference.seconds, 60)
-    print(in_minutes_and_seconds)
     return in_minutes_and_seconds[0]
 
 def parse_multiple_times(bus_times):
@@ -57,23 +70,40 @@ def parse_multiple_times(bus_times):
         bus_times[-1]
     )
 
-def create_message(bus_times):
+def create_message(bus_times, destination_name):
     count = len(bus_times)
     time_strings = [str(time) for time in bus_times]
     if count == 1:
-        return 'The next Media City UK bus is in %d minutes.' % time_strings[0]
+        return 'The next bus to %s is in %d minutes.' % (destination_name, time_strings[0])
     else:
-        return 'The next Media City UK buses leave in %s.' % parse_multiple_times(time_strings)
+        return 'The upcoming buses to %s leave in %s.' % (destination_name, parse_multiple_times(time_strings))
+
+def get_origin(arguments):
+    if len(arguments) < 2:
+        print('Must provide start location as an argument')
+        sys.exit(1)
+    origin_name = sys.argv[1]
+    allowed_values = list(stops.keys())
+    if origin_name not in allowed_values:
+        print('Origin must be one of %s' % allowed_values)
+        sys.exit(1)
+    return stops[origin_name]
 
 def main():
-    response = requests.post('https://api.stagecoachbus.com/adc/stop-monitor', headers=headers, json=data("1800NF03991"))
+    origin = get_origin(sys.argv)
+
+    response = requests.post('https://api.stagecoachbus.com/adc/stop-monitor', headers=headers, json=data(origin['id']))
     json_result = response.json()
+
     upcoming_live_buses = get_live_buses(json_result)
-    current_time = datetime.datetime.now().astimezone(british_time)
+
     arrival_times = sorted(list(map(get_arrival_time, upcoming_live_buses)))
+
+    current_time = datetime.datetime.now().astimezone(british_time)
     minutes_away = [get_difference_in_minutes_between(arrival_time, current_time) for arrival_time in arrival_times]
+
     filtered = [m for m in minutes_away if m >= 2][:3]
-    print(create_message(filtered))
+    print(create_message(filtered, origin['destination_name']))
 
 if __name__ == '__main__':
     main()
