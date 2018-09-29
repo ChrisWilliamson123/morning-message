afb53b6ad4c7f36389c2cc4f29c44bac8c032132
Chris Williamson <chris.williamson@bbc.co.uk>
2018-09-29 13:03:09 +0100

    Created a function which creates the weather message

--- create_message.py
+++ create_message.py
@@ -23,6 +23,17 @@ def create_budgeting_message(budgeting_data):
     else:
         return 'Yesterday you were over budget by %s therefore decreasing your budgeting pot balance to %s.' % (pence_to_words(saved*-1), pence_to_words(budgeting_data['pot_balance']))
 
+def create_weather_message(weather_data):
+    rain_prob = weather_data['precipitationProbability']
+    rain_message_prefix = 'a ' if rain_prob < 100 else ''
+    return 'The weather will provide %s. There will be a high of %s and a low of %s, with %s%s percent chance of rain.' % (
+        weather_data['description'],
+        weather_data['maxTemp'],
+        weather_data['minTemp'],
+        rain_message_prefix,
+        rain_prob
+    )
+
 if __name__ == "__main__":
     data = json.loads(sys.stdin.readline())
     weather_data = data['weather']
@@ -35,10 +46,10 @@ if __name__ == "__main__":
         weather_data['description'],
         weather_data['maxTemp'],
         weather_data['minTemp'],
-        weather_data['precipitationProbability']
+        'a %s' % weather_data['precipitationProbability']
     )
 
     battery_message = 'You have %s percent battery remaining.' % data['battery']
 
-    final_message = '%s %s %s %s' % (date_message, weather_message, create_budgeting_message(budgeting_data), battery_message)
+    final_message = '%s %s %s %s' % (date_message, create_weather_message(weather_data), create_budgeting_message(budgeting_data), battery_message)
     print(final_message)
